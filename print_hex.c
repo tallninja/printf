61ad9420c3a085ed046a300ab061e464ae83d91e
Ernest Wambua <ernestwambua2@gmail.com>
2022-04-19 12:44:33 +0300

    print_hex

---
+++ print_hex.c
@@ -0,0 +1,93 @@
+#include "main.h"
+
+/**
+ * print_x - prints hexadecimal numbers in lowercase
+ * @args: args
+ * Return: number of characters printed
+ */
+
+int print_x(va_list args)
+{
+	unsigned int i, j, number = va_arg(args, unsigned int);
+	char hex[256];
+
+	i = 0;
+	while (number >= 16)
+		hex[i] = number % 16, number /= 16, i++;
+
+	hex[i] = number;
+	for (j = i;; j--)
+	{
+		switch (hex[j])
+		{
+		case 10:
+			_putchar('a');
+			break;
+		case 11:
+			_putchar('b');
+			break;
+		case 12:
+			_putchar('c');
+			break;
+		case 13:
+			_putchar('d');
+			break;
+		case 14:
+			_putchar('e');
+			break;
+		case 15:
+			_putchar('f');
+			break;
+		default:
+			_putchar(hex[j] + '0');
+		}
+	}
+
+	return (i + 1);
+}
+
+/**
+ * print_X - print hexadecimal in uppercase
+ * @args: args
+ * Return: number of characters printed
+ */
+
+int print_X(va_list args)
+{
+	unsigned int i, j, number = va_arg(args, unsigned int);
+	char hex[256];
+
+	i = 0;
+	while (number >= 16)
+		hex[i] = number % 16, number /= 16, i++;
+
+	hex[i] = number;
+	for (j = i;; j--)
+	{
+		switch (hex[j])
+		{
+		case 10:
+			_putchar('A');
+			break;
+		case 11:
+			_putchar('B');
+			break;
+		case 12:
+			_putchar('C');
+			break;
+		case 13:
+			_putchar('D');
+			break;
+		case 14:
+			_putchar('E');
+			break;
+		case 15:
+			_putchar('F');
+			break;
+		default:
+			_putchar(hex[j] + '0');
+		}
+	}
+
+	return (i + 1);
+}
