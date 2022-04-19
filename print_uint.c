44a2e698cb833f867033fcef9f75bb22020b42bf
Ernest Wambua <ernestwambua2@gmail.com>
2022-04-19 12:46:18 +0300

    betty

--- print_uint.c
+++ print_uint.c
@@ -8,26 +8,26 @@
 
 int print_u(va_list args)
 {
-    unsigned int number = va_arg(args, unsigned int);
-    int last_digit = number % 10, num = number / 10;
-    int digit, base = 1, count = 1;
+	unsigned int number = va_arg(args, unsigned int);
+	int last_digit = number % 10, num = number / 10;
+	int digit, base = 1, count = 1;
 
-    if (num) /* checks to see if num > 10 */
-    {
-        while (num / 10)                 /* loop until num < 10 */
-            base = base * 10, num /= 10; /* calculate base of the number */
+	if (num) /* checks to see if num > 10 */
+	{
+		while (num / 10)				 /* loop until num < 10 */
+			base = base * 10, num /= 10; /* calculate base of the number */
 
-        num = number / 10; /* reset the value of num to it's original value */
-        while (base > 0)
-        {
-            digit = num / base;
-            _putchar(digit + '0');
-            num -= (digit * base);
-            base /= 10;
-            count++;
-        }
-    }
+		num = number / 10; /* reset the value of num to it's original value */
+		while (base > 0)
+		{
+			digit = num / base;
+			_putchar(digit + '0');
+			num -= (digit * base);
+			base /= 10;
+			count++;
+		}
+	}
 
-    _putchar(last_digit + '0');
-    return (count);
+	_putchar(last_digit + '0');
+	return (count);
 }
